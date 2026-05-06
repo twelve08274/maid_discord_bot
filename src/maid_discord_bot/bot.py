5c97689038e2104f50bb51adc62570dcfe07d9ed
AMkuro <4324018@ed.tus.ac.jp>
2026-05-06 13:01:04 +0900

    ライブラリ名が変わるとき用に相対インポート

--- src/maid_discord_bot/bot.py
+++ src/maid_discord_bot/bot.py
@@ -1,9 +1,9 @@
 import discord
 from discord.ext import commands
 
-from maid_discord_bot.commands.ping import register_ping_command
-from maid_discord_bot.commands.register import register_register_command
-from maid_discord_bot.config import (
+from .commands.ping import register_ping_command
+from .commands.register import register_register_command
+from .config import (
     get_discord_test_guild_id,
     get_discord_token,
 )

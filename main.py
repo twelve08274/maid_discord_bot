0f33647a02f7347b078bb132461f1331a82f0d32
AMkuro <4324018@ed.tus.ac.jp>
2026-05-07 16:50:24 +0900

    fix: the relative import

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import sys
-from pathlib import Path
-
-
-sys.path.insert(0, str(Path(__file__).resolve().parent / "src"))
-
-from maid_discord_bot.bot import run_bot  # noqa: E402
+from src.bot import run_bot  # noqa: E402
 
 
 if __name__ == "__main__":
     run_bot()
-
